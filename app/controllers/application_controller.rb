ccb28eece1cdd5dc637c525d5e330c09f53e2fcd
Daniel Moreto <dfmoreto@gmail.com>
2019-11-24 02:12:12 -0300

    Página de Cadastro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_devise_params, if: :devise_controller?
+
+  protected
+  
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
