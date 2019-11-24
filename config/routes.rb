f36312255edda80bb2db0888587a79121cd6d5d1
Daniel Moreto <dfmoreto@gmail.com>
2019-11-24 06:11:07 -0300

    Listar chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "chats#index"
 
   resources :contacts, only: %i(index new create destroy)
   resources :chats, only: %i(index show) do
