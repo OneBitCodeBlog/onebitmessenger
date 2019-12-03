a1d15f1c1bc008f15632ef12c20e59746fb4e662
Daniel Moreto <dfmoreto@gmail.com>
2019-12-03 19:48:46 -0300

    Configurando o PWA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,11 @@ document.addEventListener("turbolinks:load", function() {
     }
   })
 })
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+      console.log(reg);
+    });
+}
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "chats#index"
 
+  get "/manifest.json" => "service_workers#manifest"
+  get "/service-worker.js" => "service_workers#service_worker"
+
   resources :contacts, only: %i(index new create destroy)
   resources :chats, only: %i(index show) do
     resources :messages, only: :create, defaults: { format: :js }
