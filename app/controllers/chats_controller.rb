f91fcb8ba437155439306dede207b7dc498c38b9
Daniel Moreto <dfmoreto@gmail.com>
2019-12-04 01:39:35 -0300

    Listar chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,10 @@
 class ChatsController < ApplicationController
+  def index
+    @users = User.joins(:sent).where(messages: { receiver_id: current_user })
+    @users += User.joins(:received).where(messages: { sender_id: current_user })
+    @users.uniq!
+  end
+  
   def show
     @user = User.find(params[:id])
     @messages = Message.where(sender: current_user, receiver: @user)
